41254807c8bda7f539f549775d367bffaac6009c
Nazımhan Fıçıcı <nazimhanficici@gmail.com>
2022-02-02 16:23:01 +0300

    dummy

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -48,49 +48,4 @@ const NavbarComponent = React.forwardRef((props, ref)=>{
     ) 
 })
 
-// function NavbarComponent() {
-
-    // const [url, setUrl] = useState("")
-    // console.log(url);
-    // const urlRef = useRef();
-    // console.log(urlRef);
-    
-    // useEffect(()=>{
-    //     setUrl(window.location.href)
-    //     console.log("heyyy")
-    //     if(url.includes("more")){
-    //         addMore();
-    //     }
-    //     else{
-    //         addHome();
-    //     }
-
-    // },[])
-    // useEffect(()=>{
-    //     console.log("aldsg")
-    // },[url])
-
-    // const addHome = () => {
-
-    //     urlRef.current.classList.remove("atmore");
-    //     urlRef.current.classList.add("athome");
-    // }
-    // const addMore = () => {
-
-    //     urlRef.current.classList.remove("athome");
-    //     urlRef.current.classList.add("atmore");
-    // }
-
-    // return (
-    //     <div ref={urlRef} className="navbar-custom athome">
-    //         <Link id="name-logo"to="#">NAZIMHAN FIÇICI</Link>
-    //         <Link onClick={addHome} to ="/home">Home</Link>
-    //         <Link onClick={addMore} to ="/more#aboutme">About Me</Link>
-    //         <Link onClick={addMore} to ="/more#skills">Skills</Link>
-    //         <Link onClick={addMore} to ="/more#projects" >Projects</Link>
-    //         <Link  to="#contact">Contact</Link>    
-    //     </div>
-    // )
-
-
 export default NavbarComponent;
