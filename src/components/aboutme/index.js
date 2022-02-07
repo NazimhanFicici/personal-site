d0ee590d6d8302938e66ef550389cc237ce04118
Nazımhan Fıçıcı <nazimhanficici@gmail.com>
2022-02-07 15:38:28 +0300

    height-padding

--- src/components/aboutme/index.js
+++ src/components/aboutme/index.js
@@ -9,11 +9,14 @@ function Aboutme() {
                 <p>
                 I am Nazımhan, I graduated from Ankara university, major of computer engineering. I started my major education in 2016 since then in order to expand my vision and capabilities I always sought new ways to improve myself.
                 <br />
+                <br />
                 Due to my past experiences I can declare myself as a quick learner, curious and innovative person. Since my childhood, i have always wanted to turn nothing into something, when it comes to make a big decisions i have always made my decisions based on this idea.
+                <br />
                 <br />
                 Speaking of teamwork and social skills of mine, I can honestly say that i am enthusiastic about taking role in events that requires cooperation and collaboration. And my past experiences can be shown as an evidence on this topic. Such as acting in Theater Club, both in primary school and high school, doing team sports such as Soccer in an amateur club and participating school team, i was also in Basketball team of my primary school and i have played American Football in university.
                 <br />
-                Since the early years of my educational life. The years that I have spent in team sports taught me to be a disciplined person, and I experienced how rewarding it is to overcome the hardships with people that I share the same goal. In my opinion, the core factor of a successful project is being passionate about it because it brings sparing more time to it and creativity with itself.
+                <br />
+                Since the early years of my educational life. The years that I have spent in team sports taught me to be a disciplined person, and I experienced how rewarding it is to overcome the hardships with people that I share the same goal.
                 </p>
             </div>
         </div>
